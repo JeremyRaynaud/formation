c9abf8c071868913c60f3c8f4f8d723d8bfd85b9
Leon-Kng <92827382+Leon-Kng@users.noreply.github.com>
2024-11-07 16:52:13 +0100

    Update get_data.R

--- get_data.R
+++ get_data.R
@@ -2,13 +2,24 @@ library("readr")
 library("tidyverse")
 library("FUNGuildR")
 
+
+occ_colombia <- rgbif::occ_search(country = "CO",
+                                 elevation = '0,1500',
+                                 kingdomKey = 5,
+                                 year = '2023,2024',
+                                 limit = 30000)
+view(occ_colombia$data)
+
 data <- read_rds("data/data_raw/bosque_fung_annot_clean_agg.rds")
 
 motus_df <- data$motus |> 
   rename(Taxonomy = `Full classification`) |> 
   funguild_assign() |> 
   select(c(lineage_dnabarcoder, 79:89)) |> 
-  mutate(trophicMode = str_trim(colonne_texte, side = "left"))
+  mutate(trophicMode = str_trim(trophicMode, side = "left")) |> 
+  unique()
 
 table(motus_df$trophicMode)
 write.csv(motus_df, "data/data_modified/motus_bosque_fung_annot_clean_agg_func.csv")
+
+unique(motus_df$taxon)
